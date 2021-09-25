d13ec280553fba5b5dbf190e68e85f4098e27b5d
EinYs <ysunny32@gmail.com>
2021-09-26 02:56:41 +0900

    arbiter

--- main/init/setReplication.js
+++ main/init/setReplication.js
@@ -1,9 +1,9 @@
 // init/ 
-config = { _id: "mongoset", 
+config = { _id: "rs1", 
     members: 
     [{ _id: 0, host: "mainhost:30001" }, 
     { _id: 1, host: "mongohost:30002" }, 
-    { _id: 2, host: "mongohost:30003" },] 
+    { _id: 2, host: "mongohost:30003", "arbiterOnly": true},] 
 }
 rs.initiate(config);
 rs.conf();
