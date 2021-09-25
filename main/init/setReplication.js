ee65040462394c797991b1677724bd209fa85756
EinYs <ysunny32@gmail.com>
2021-09-26 01:59:16 +0900

    script

--- main/init/setReplication.js
+++ main/init/setReplication.js
@@ -1,9 +1,9 @@
 // init/ 
 config = { _id: "mongoset", 
     members: 
-    [{ _id: 0, host: "mongo1:27017" }, 
-    { _id: 1, host: "mongohost:27017" }, 
-    { _id: 2, host: "mongohost:27017" },] 
+    [{ _id: 0, host: "mainhost:30001" }, 
+    { _id: 1, host: "mongohost:30002" }, 
+    { _id: 2, host: "mongohost:30003" },] 
 }
 rs.initiate(config);
 rs.conf();
