6d9a8dafbbe23beb96606361c09bc5534bbd0558
EinYs <ysunny32@gmail.com>
2021-09-26 01:33:27 +0900

    set rs ip

--- main/init/setReplication.js
+++ main/init/setReplication.js
@@ -2,8 +2,8 @@
 config = { _id: "mongoset", 
     members: 
     [{ _id: 0, host: "mongo1:27017" }, 
-    { _id: 1, host: "mongo2:27017" }, 
-    { _id: 2, host: "mongo3:27017" },] 
+    { _id: 1, host: "mongohost:27017" }, 
+    { _id: 2, host: "mongohost:27017" },] 
 }
 rs.initiate(config);
 rs.conf();
